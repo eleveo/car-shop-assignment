88a39a877a080bb1e5a1484149e588f84537c8f0
Petr Cincibus <petr.cincibus@eleveo.com>
2023-01-25 13:18:16 +0100

    empty project setup

---
+++ src/main/java/com/eleveo/carshophomework/CarShopHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.eleveo.carshophomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarShopHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarShopHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eleveo/carshophomework/CarShopHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eleveo.carshophomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarShopHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
