7836fc658d179b3d81d8a57c8995951957c634ce
Ladislava Frckova <ladislava.frckova@eleveo.com>
2023-01-25 15:46:43 +0100

    prepare dummy implementation for the shopper

---
+++ car-shopper/src/main/java/com/eleveo/carshophomework/shopper/DummyCarShopper.java
@@ -0,0 +1,20 @@
+package com.eleveo.carshophomework.shopper;
+
+import com.eleveo.carshop.client.ApiClient;
+import com.eleveo.carshop.client.DefaultApi;
+import com.eleveo.carshop.client.model.Car;
+import com.eleveo.carshop.client.model.Order;
+
+import java.util.UUID;
+
+public class DummyCarShopper implements CarShopper {
+
+	@Override
+	public void selectAndBuyCars(String serverUri) {
+		DefaultApi defaultApi = new DefaultApi(new ApiClient().setBasePath(serverUri));
+		Car car = defaultApi.getCars().block().getCars().get(0);
+		Order order = new Order();
+		order.setCarId(car.getCarId());
+		defaultApi.orderCar(order).block();
+	}
+}
--- car-shopper/src/test/java/com/eleveo/carshophomework/CarShopHomeworkApplicationTests.java
+++ car-shopper/src/test/java/com/eleveo/carshophomework/CarShopHomeworkApplicationTests.java
@@ -1,31 +1,56 @@
 package com.eleveo.carshophomework;
 
+import com.eleveo.carshophomework.shopper.CarShopper;
+import com.eleveo.carshophomework.shopper.DummyCarShopper;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.reactive.function.client.ClientResponse;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static org.assertj.core.api.Assertions.assertThat;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.noContent;
+import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 
 @SpringBootTest
 @WireMockTest(httpPort = 8444)
 class CarShopHomeworkApplicationTests {
 
-    private WebClient webClient = WebClient.builder()
-            .baseUrl("http://localhost:8444/")
-            .build();
+	private static final String BASE_URL = "http://localhost:8444/";
+	CarShopper carShopper = new DummyCarShopper();
 
-    @Test
-    void contextLoads() {
-        stubFor(get("/cars").willReturn(ok()));
-        ClientResponse response = webClient.get()
-                .uri("/cars")
-                .exchange()
-                .block();
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK);
-    }
+	//language=json
+	String car = """
+			{
+			  "cars": [
+			    {
+			      "carId": "822ac97f-1202-4399-95f5-a4a4a183f20a",
+			      "manufacturer": "Tesla",
+			      "model": "Model X Plaid",
+			      "engineType": "ELECTRIC",
+			      "powerWatts": 373746,
+			      "heightMeters": 1.1,
+			      "widthMeters": 2.1,
+			      "lengthMeters": 5.5,
+			      "price": 299000,
+			      "priceCurrency": "USD"
+			    }
+			  ]
+			}
+					""";
+
+	@Test
+	void singleCarOrder() {
+		stubFor(get("/cars").willReturn(okJson(car)));
+		stubFor(post("/orders").willReturn(noContent()));
+		carShopper.selectAndBuyCars(BASE_URL);
+		verify(postRequestedFor(urlPathEqualTo("/orders"))
+				.withRequestBody(equalToJson("{\"carId\": \"822ac97f-1202-4399-95f5-a4a4a183f20a\"}")));
+
+	}
 
 }
