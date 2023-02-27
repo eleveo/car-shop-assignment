4ba7b372ece8613c002912c173a507282b821e45
asd <asd@asds-MacBook-Air.local>
2023-02-27 23:26:37 +0100

    test added

--- car-shopper/src/test/java/com/eleveo/carshophomework/CarShopHomeworkApplicationTests.java
+++ car-shopper/src/test/java/com/eleveo/carshophomework/CarShopHomeworkApplicationTests.java
@@ -2,6 +2,7 @@ package com.eleveo.carshophomework;
 
 import com.eleveo.carshophomework.shopper.CarShopper;
 import com.eleveo.carshophomework.shopper.DummyCarShopper;
+import com.eleveo.carshophomework.shopper.SelectiveCarShopper;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +16,8 @@ class CarShopHomeworkApplicationTests {
 	private static final String BASE_URL = "http://localhost:8444/";
 	CarShopper carShopper = new DummyCarShopper();
 
+	CarShopper selectiveCarShopper = new SelectiveCarShopper();
+
 	@Test
 	void singleCarOrder() {
 		stubFor(get("/cars").willReturn(ok()
@@ -26,6 +29,27 @@ class CarShopHomeworkApplicationTests {
 		verify(postRequestedFor(urlPathEqualTo("/orders"))
 				.withRequestBody(equalToJson("{\"carId\": \"79c7faaf-d0af-304e-94aa-6e426ae1d07a\"}")));
 
+	}
+
+	@Test
+	void selectiveCarOrder() {
+		stubFor(get("/cars").willReturn(ok()
+				.withHeader("Content-Type", "application/json")
+				.withBodyFile("cars.json")
+		));
+		stubFor(post("/orders").willReturn(noContent()));
+		selectiveCarShopper.selectAndBuyCars(BASE_URL);
+		verify(5, postRequestedFor(urlEqualTo("/orders")));
+		verify(postRequestedFor(urlPathEqualTo("/orders"))
+				.withRequestBody(equalToJson("{\"carId\": \"23c49169-c73a-328e-9b92-d5018d8a235e\"}")));
+		verify(postRequestedFor(urlPathEqualTo("/orders"))
+				.withRequestBody(equalToJson("{\"carId\": \"29b64088-2805-368f-b50a-b35b7a6b9655\"}")));
+		verify(postRequestedFor(urlPathEqualTo("/orders"))
+				.withRequestBody(equalToJson("{\"carId\": \"94733846-bfa6-3aaf-8b1d-199a1dc187b7\"}")));
+		verify(postRequestedFor(urlPathEqualTo("/orders"))
+				.withRequestBody(equalToJson("{\"carId\": \"c5e70584-9505-3acb-a23a-1891fec4d223\"}")));
+		verify(postRequestedFor(urlPathEqualTo("/orders"))
+				.withRequestBody(equalToJson("{\"carId\": \"26a2664c-a44f-3b86-91d5-bbd4cf3cdda0\"}")));
 	}
 
 }
